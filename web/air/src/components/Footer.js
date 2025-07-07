30838a1b4968509afd6f6df1fd3e6af153f91a63
liuqinwang <you@example.com>
2025-07-07 09:12:26 +0800

    更新Web前端文件（air主题）

--- web/air/src/components/Footer.js
+++ web/air/src/components/Footer.js
@@ -28,36 +28,37 @@ const Footer = () => {
   }, []);
 
   return (
-    <Segment vertical>
-      <Container textAlign='center'>
-        {footer ? (
-          <div
-            className='custom-footer'
-            dangerouslySetInnerHTML={{ __html: footer }}
-          ></div>
-        ) : (
-          <div className='custom-footer'>
-            <a
-              href='https://github.com/songquanpeng/one-api'
-              target='_blank'
-            >
-              {systemName} {process.env.REACT_APP_VERSION}{' '}
-            </a>
-            由{' '}
-            <a href='https://github.com/songquanpeng' target='_blank'>
-              JustSong
-            </a>{' '}
-            构建，主题 air 来自{' '}
-            <a href='https://github.com/Calcium-Ion' target='_blank'>
-              Calon
-            </a>{' '}，源代码遵循{' '}
-            <a href='https://opensource.org/licenses/mit-license.php'>
-              MIT 协议
-            </a>
-          </div>
-        )}
-      </Container>
-    </Segment>
+    <br/>
+    // <Segment vertical>
+    //   <Container textAlign='center'>
+    //     {footer ? (
+    //       <div
+    //         className='custom-footer'
+    //         dangerouslySetInnerHTML={{ __html: footer }}
+    //       ></div>
+    //     ) : (
+    //       <div className='custom-footer'>
+    //         <a
+    //           href='https://github.com/songquanpeng/one-api'
+    //           target='_blank'
+    //         >
+    //           {systemName} {process.env.REACT_APP_VERSION}{' '}
+    //         </a>
+    //         由{' '}
+    //         <a href='https://github.com/songquanpeng' target='_blank'>
+    //           JustSong
+    //         </a>{' '}
+    //         构建，主题 air 来自{' '}
+    //         <a href='https://github.com/Calcium-Ion' target='_blank'>
+    //           Calon
+    //         </a>{' '}，源代码遵循{' '}
+    //         <a href='https://opensource.org/licenses/mit-license.php'>
+    //           MIT 协议
+    //         </a>
+    //       </div>
+    //     )}
+    //   </Container>
+    // </Segment>
   );
 };
 
